be861102db272a9c0eebabebee081127c99cb835
Koki Takahashi <hakatasiloving@gmail.com>
2026-03-06 22:29:26 +0900

    Implement participants feature in rooms: update Firestore rules, schema, and UI

--- src/lib/schema.d.ts
+++ src/lib/schema.d.ts
@@ -16,5 +16,8 @@ export interface Room extends DocumentData {
 	name: string;
 	createdAt: Timestamp;
 	createdBy: string;
-	participants: Record<string, Timestamp>;
+
+	/** Represents the participants in the room, where the key is the user ID and the value is the timestamp of when they updated their participation.
+	 * Using serverTimestamp() will result in null until the document is fetched, so we need to account for that in the type */
+	participants: Record<string, Timestamp | null>;
 }
--- src/routes/rooms/[roomId]/index.tsx
+++ src/routes/rooms/[roomId]/index.tsx
@@ -1,18 +1,98 @@
-import type {Component} from 'solid-js';
-import {Rooms} from '~/lib/firebase';
-import {useFirestore} from 'solid-firebase';
-import {doc} from 'firebase/firestore';
+import {
+	createEffect,
+	For,
+	onCleanup,
+	Show,
+	createSignal,
+	type Component,
+} from 'solid-js';
+import {auth, Rooms} from '~/lib/firebase';
+import {useAuth, useFirestore} from 'solid-firebase';
+import {doc, serverTimestamp, updateDoc} from 'firebase/firestore';
 import {useParams} from '@solidjs/router';
+import Doc from '~/lib/Doc';
+
+import styles from './index.module.css';
 
 const Index: Component = () => {
 	const params = useParams();
-	const room = useFirestore(doc(Rooms, params.roomId));
+	const roomRef = doc(Rooms, params.roomId);
+	const room = useFirestore(roomRef);
+	const authState = useAuth(auth);
+
+	const updateTimestamp = () => {
+		if (!authState.data || !room.data) {
+			return;
+		}
+
+		console.log('Updating timestamp for user', authState.data.uid);
+
+		updateDoc(roomRef, {
+			[`participants.${authState.data.uid}`]: serverTimestamp(),
+		});
+	};
+
+	createEffect(() => {
+		updateTimestamp();
+	});
+
+	const [isOnlineTable, setIsOnlineTable] = createSignal<
+		Record<string, boolean>
+	>({});
+
+	const onTick = () => {
+		if (room.data) {
+			const now = Date.now();
+			const newIsOnlineTable: Record<string, boolean> = {};
+			for (const [uid, timestamp] of Object.entries(
+				room.data.participants || {},
+			)) {
+				if (timestamp) {
+					const lastUpdated = timestamp.toMillis();
+					newIsOnlineTable[uid] = now - lastUpdated < 30 * 1000;
+				} else {
+					newIsOnlineTable[uid] = false;
+				}
+			}
+			setIsOnlineTable(newIsOnlineTable);
+		}
+
+		updateTimestamp();
+	};
+
+	const intervalId = setInterval(onTick, 10 * 1000);
+	onCleanup(() => {
+		clearInterval(intervalId);
+	});
 
 	return (
 		<div>
-			<h1>{room.data?.name}</h1>
-			<p>Created at: {room.data?.createdAt?.toDate().toLocaleString()}</p>
-			<p>Created by: {room.data?.createdBy}</p>
+			<Doc data={room}>
+				{(roomData) => (
+					<>
+						<h1>{roomData.name}</h1>
+						<p>Created at: {roomData.createdAt?.toDate()?.toLocaleString()}</p>
+						<p>Created by: {roomData.createdBy}</p>
+						<ul class={styles.participants}>
+							<For each={Object.keys(roomData.participants)}>
+								{(uid) => (
+									<li
+										classList={{
+											[styles.participant]: true,
+											[styles.online]: isOnlineTable()[uid],
+										}}
+									>
+										<Show when={authState.data?.uid === uid}>
+											<div class={styles.youBadge}>You</div>
+										</Show>
+										<div class={styles.participantUid}>UID: {uid}</div>
+									</li>
+								)}
+							</For>
+						</ul>
+					</>
+				)}
+			</Doc>
 		</div>
 	);
 };
